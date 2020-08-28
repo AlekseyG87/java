98110c032dd0b79af0074d26439289d38a592331
GAleksey <GAleksey1987@yandex.ru>
2020-08-28 20:19:16 +0800

    !

--- Java2_les1/src/Background.java
+++ Java2_les1/src/Background.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.awt.*;
 
-public class Background {
+public class Background extends Sprite  {
+
+    private final Color color;
+    double col;
+    double newCol;
+
+
+    Background() {
+        color = new Color(
+                (int) (newCol=Math.random() * 255), //r
+                (int) (newCol=Math.random() * 255), //g
+                (int) (newCol=Math.random() * 255)  //b
+        );
+    }
+
+    public void update(GameCanvas canvas, float deltaTime) {
+        newCol += col * deltaTime;
+    }
+
+
+    public void render(GameCanvas canvas, Graphics g) {
+        g.setColor(color);
+    }
 }
